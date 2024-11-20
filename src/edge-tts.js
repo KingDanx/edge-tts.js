20b4a6292eef442d02600700d4a4efd4e48009e7
Dan Prudhomme <dannyprud@yahoo.com>
2024-11-20 16:26:14 -0600

    make ttsToFile async and resolve filePath

--- src/edge-tts.js
+++ src/edge-tts.js
@@ -2,7 +2,7 @@ import { buildWebSocketURL } from "./utils.js";
 import fs from "fs";
 import TTS from "./tts.js";
 import constants from "./constants.js";
-import path from "path";
+import path, { resolve } from "path";
 import WebSocket from "ws";
 
 export default class EdgeTTS {
@@ -31,66 +31,68 @@ export default class EdgeTTS {
 
   /**
    * @param {string} directory
-   * @returns {void}
+   * @returns {Promise}
    */
   ttsToFile(directory = "") {
-    const socket = new WebSocket(this.url, {
-      headers: constants.WSS_HEADERS,
-    });
+    return new Promise((resolve, reject) => {
+      const socket = new WebSocket(this.url, {
+        headers: constants.WSS_HEADERS,
+      });
 
-    socket.binaryType = "arraybuffer";
+      socket.binaryType = "arraybuffer";
 
-    socket.addEventListener("error", (e) => {
-      socket.close();
-    });
+      socket.addEventListener("error", (e) => {
+        socket.close();
+        reject(e);
+      });
 
-    socket.addEventListener("close", (e) => {
-      const endIndex = this.tts.text.length > 20 ? 20 : this.tts.text.length;
-      const fileName = `${this.tts.voice}-${this.tts.text
-        .slice(0, endIndex)
-        .trim()}${this.tts.fileType.ext}`;
-      const filePath = path.join(directory, fileName);
+      socket.addEventListener("close", (e) => {
+        const endIndex = this.tts.text.length > 20 ? 20 : this.tts.text.length;
+        const fileName = `${this.tts.voice}-${this.tts.text
+          .slice(0, endIndex)
+          .trim()}${this.tts.fileType.ext}`;
+        const filePath = path.join(directory, fileName);
 
-      fs.writeFile(filePath, this.file, (err) => {
-        if (err) {
-          console.error("Error writing file:", err);
+        fs.writeFile(filePath, this.file, (err) => {
+          if (err) {
+            console.error("Error writing file:", err);
+          }
+          resolve(filePath);
+        });
+      });
+
+      socket.addEventListener("open", () => {
+        socket.send(this.tts.generateCommand());
+        socket.send(this.tts.generateSSML());
+      });
+
+      socket.addEventListener("message", (data) => {
+        if (data.data instanceof ArrayBuffer) {
+          const buffer = Buffer.from(data.data);
+          if (buffer.length >= 2) {
+            const headerLength = buffer.readUInt16BE(0) + "\r\n".length;
+            const header = buffer.subarray(0, headerLength);
+            const result = this.parseMessageText(header.toString());
+
+            if (result.Path !== "audio") {
+              return;
+            }
+
+            const payload = buffer.subarray(headerLength);
+            const totalLength = this.file.length + payload.length;
+            this.file = Buffer.concat([this.file, payload], totalLength);
+          } else {
+            console.error(
+              "Received data is too short to contain a valid header."
+            );
+          }
+        } else if (typeof data.data === "string") {
+          const result = this.parseMessageText(data.data);
+          if (result.Path === "turn.end") {
+            socket.close(0);
+          }
         }
       });
-    });
-
-    socket.addEventListener("open", () => {
-      socket.send(this.tts.generateCommand());
-      socket.send(this.tts.generateSSML());
-    });
-
-    socket.addEventListener("message", (data) => {
-      if (data.data instanceof ArrayBuffer) {
-        const buffer = Buffer.from(data.data);
-        if (buffer.length >= 2) {
-          const headerLength = buffer.readUInt16BE(0) + "\r\n".length;
-          const header = buffer.subarray(0, headerLength);
-          const result = this.parseMessageText(header.toString());
-
-          if (result.Path !== "audio") {
-            return;
-          }
-
-          const payload = buffer.subarray(headerLength);
-          const totalLength = this.file.length + payload.length;
-          this.file = Buffer.concat([this.file, payload], totalLength);
-        } else {
-          console.error(
-            "Received data is too short to contain a valid header."
-          );
-        }
-      } else if (typeof data.data === "string") {
-        const result = this.parseMessageText(data.data);
-        console.log(result);
-        if (result.Path === "turn.end") {
-          socket.close(0);
-        }
-      } else {
-      }
     });
   }
 
